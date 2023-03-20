1324b93d97c093daccd620f6a805de553b66cdc0
Ahmad Zahraman <ahmad.zahraman4159@gmail.com>
2023-03-20 20:22:23 +0200

    Enable CORS

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+var cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3030;
 
 // your code
+app.use(cors())
 app.get('/', (req, res) => {
     res.send('Hi All');
 })
